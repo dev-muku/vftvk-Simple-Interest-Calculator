c2ba7fb02b5d11203fd80f57f548b7781f20a25a
dev-muku <98166980+dev-muku@users.noreply.github.com>
2022-01-25 20:42:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,13 +12,14 @@ function compute() //Compute Interest function
     //Printing result only when principal amount is greater than zero.
     if(principal > 0) {
         var result = document.getElementById("result");
-        result.innerHTML = "\<br \/\>If you deposit \<mark\>"+
-        principal+"\<\/mark\>,\<br \/\>at an interest rate of \<mark\>"+
-        rate+"\%\<\/mark\>.\<br\/\>You will receive an amount of \<mark\>"+
-        interest+"\<\/mark\>,\<br\/\>in the year \<mark\>"+year+"\<\/mark\>\<br \/\>";
+        result.innerHTML = "<br />If you deposit <mark>"+
+        principal+"</mark>,<br />at an interest rate of <mark>"+
+        rate+"%</mark>.<br />You will receive an amount of <mark>"+
+        interest+"</mark>,<br />in the year <mark>"+year+"</mark><br />";
     }
-    else if (principal == 0) {
-        alert("Principal amout field is empty.\nPlease enter a positive number!");
+    else if (principal <= 0) {
+        alert("Enter a positive number.");
+        document.getElementById("principal").value = '';
         document.getElementById("principal").focus();
     }
 }
@@ -26,17 +27,5 @@ function compute() //Compute Interest function
 function updateRate() //Showing dynamically changed value for Rate of Interest. 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval + "\%";
-}
-
-function validate_principal() // Validating principal amout field.
-{
-    let principal = document.getElementById("principal");
-    //Alert if user enters zero or negative values in principal input field.
-    if (principal.value <= 0) {
-        alert("Enter a positive number");
-        principal.value = '';
-        principal.focus();
-    }
-
+    document.getElementById("rate_val").innerText = rateval + "%";
 }
